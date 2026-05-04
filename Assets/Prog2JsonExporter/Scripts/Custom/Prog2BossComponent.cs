dc9bc02ef564ed143f0aca4ab57fc908e7d9bc93
Rei-Marcel <marcel.rei@student.howest.be>
2026-05-04 21:40:40 +0200

    Added Boss enemy data

---
+++ Assets/Prog2JsonExporter/Scripts/Custom/Prog2BossComponent.cs
@@ -0,0 +1,23 @@
+﻿using Prog2JsonExporter.Scripts.Data;
+using Prog2JsonExporter.Scripts.SceneEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Prog2JsonExporter.Scripts.Example
+{
+    public class Prog2BossComponent: Prog2CustomObjectComponent
+    {
+        [SerializeField] private Transform leftBorder;
+        [SerializeField] private Transform rightBorder;
+        private Prog2BossData customData;
+       
+        public override Prog2CustomData GetCustomData()
+        {
+            customData = new Prog2BossData();
+            customData.entityID = 4;
+            customData.leftBorder = leftBorder.position.x;
+            customData.rightBorder = rightBorder.position.x;
+            return customData;
+        }
+    }
+}
---
+++ Assets/Prog2JsonExporter/Scripts/Custom/Prog2BossData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Prog2JsonExporter.Scripts.Data;
+using UnityEngine;
+
+namespace Prog2JsonExporter.Scripts.Example
+{
+    [Serializable]
+    public class Prog2BossData : Prog2CustomData
+    {
+        public int entityID = 0;
+        public float leftBorder;
+        public float rightBorder;
+    }
+}
